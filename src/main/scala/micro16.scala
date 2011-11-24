a20d5a273f631fc2d7d2902247bdf7f5f0c8443f
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2011-11-24 20:54:13 +0100

    added memory access

--- src/main/scala/micro16.scala
+++ src/main/scala/micro16.scala
@@ -34,23 +34,29 @@ case class ifNegative(cr: Register, n: Boolean, t: Label) extends FlowControl(cr
 case class StatementSequence(statements: List[Statement]) extends Statement {
 	override def execute { statements.foreach(_.execute) }
 }
-case class FunctionName(name: String) extends Statement
+class MemoryAccess extends Statement
+case class ReadMemory extends MemoryAccess {
+	override def execute { State.MBR = State.flash(State.MAR) }
+}
+case class WriteMemory extends MemoryAccess {
+	override def execute { State.flash(State.MAR) = State.MBR }
+}
 
 class Micro16Parser extends JavaTokenParsers {
 	def statement: Parser[Statement] =  ( 
-			label 
-			| flowControl 
-			| assignment~";"~statement ^^ { 
-				case ass~";"~(fun: StatementSequence) => StatementSequence(ass :: fun.statements)
-				case ass~";"~fun => StatementSequence(List(ass, fun)) } 
-			| assignment 
-			| functionName 
-			| failure("illegal statement"))
+		label 
+		| flowControl 
+		| assignment~";"~statement ^^ { 
+			case ass~";"~(fun: StatementSequence) => StatementSequence(ass :: fun.statements)
+			case ass~";"~fun => StatementSequence(List(ass, fun)) } 
+		| assignment 
+		| memoryAccess 
+		| failure("illegal statement"))
 	def assignment: Parser[Assignment] = register~"<-"~value ^^ { case r~"<-"~v => Assignment(r, v) }
 	def register: Parser[Register] = ( 
-							"R"~"""\d{1,2}""".r ^^ { case "R"~num => Register(15.min(num.toInt)) }
-							| "MAR" ^^ { case _ => Register(16) } 
-							| "MBR" ^^ { case _ => Register(17) } )
+		"R"~"""\d{1,2}""".r ^^ { case "R"~num => Register(State.maxRegister.min(num.toInt)) }
+		| "MAR" ^^ { case _ => Register(State.pMAR) } 
+		| "MBR" ^^ { case _ => Register(State.pMBR) } )
 	def value: Parser[Value] = register | function
 	def expression: Parser[Value] = ( register~"+"~register ^^ { case r1~"+"~r2 => Addition(r1, r2) } 
 		| register 
@@ -60,31 +66,43 @@ class Micro16Parser extends JavaTokenParsers {
 		| "rsh("~>expression<~")" ^^ (RightShift(_)) )
 	def label: Parser[Label] = labelName<~":"
 	def labelName: Parser[Label] = "[A-Z]".r ^^ (Label(_))
-	def functionName: Parser[FunctionName] = "[a-z]+".r ^^ (FunctionName(_))
-	def flowControl: Parser[FlowControl] = ( "("~negate~register~"); if"~condType~"goto"~labelName ^^ { 
-		case "("~neg~reg~"); if"~condT~"goto"~label => 
-			if(condT == "Z") 
-				ifZero(reg, neg, label) 
-			else 
-				ifNegative(reg, neg, label) } )
+	def memoryAccess: Parser[MemoryAccess] = "rd" ^^ (m => ReadMemory() ) | "wr" ^^ (m => WriteMemory() )
+	def flowControl: Parser[FlowControl] = ( 
+		"("~negate~register~"); if"~condType~"goto"~labelName ^^ { 
+			case "("~neg~reg~"); if"~condT~"goto"~label => 
+				if(condT == "Z") 
+					ifZero(reg, neg, label) 
+				else 
+					ifNegative(reg, neg, label) } )
 	def negate: Parser[Boolean] = "-" ^^ (x => true) | "" ^^ (x => false)
 	def condType: Parser[String] = "Z" | "N"
 }
 
 object State {
-	val registers = (0 to 17).map(n => 0xDEADBEEF).toArray
+	val maxRegister = 15
+	val registers = (0 to (maxRegister + 2)).map(n => 0xDEADBEEF).toArray
 	registers(0) =  0
 	registers(1) =  1
 	registers(2) = -1
+	val pMAR = 16
+	val pMBR = 17
+	def MAR = registers(pMAR)
+	def MAR_= (newVal: Int) { registers(pMAR) = newVal }
+	def MBR = registers(pMBR)
+	def MBR_= (newVal: Int) { registers(pMBR) = newVal }
+	// let's assume flash has a size of 1024 dwords
+	val flash = (0 to 1023).map(n => 0xDEADBEEF).toArray
 
 	def dump() {
 		println()
 		println("+++++++ Dumping State +++++++")
 		println("Execution Pointer: %02d".format(execPointer))
 		println("====== Registers ======")
-		registers.take(16).zipWithIndex.foreach(l => println(" %02d: %s".format(l._2, l._1)))
-		println("MAR: %s".format(registers(16)))
-		println("MBR: %s".format(registers(17)))
+		registers.take(maxRegister+1).zipWithIndex.foreach(l => println(" %02d: %s".format(l._2, l._1)))
+		println("MAR: %s".format(MAR))
+		println("MBR: %s".format(MBR))
+		println("====== Beginning of flash =====")
+		flash.take(20).zipWithIndex.foreach(l => println(" %02d: %s".format(l._2, l._1)))
 		println("+++++++++++++++++++++++++++++")
 		println()
 	}
