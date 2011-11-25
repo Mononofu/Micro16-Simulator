1bc71b500a51e487fbd463d05e97cfa0dab520d4
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2011-11-25 19:51:10 +0100

    fixed state-machine, added reload code button to GUI

--- src/main/scala/gui.scala
+++ src/main/scala/gui.scala
@@ -17,6 +17,7 @@ object Micro16SimulatorGUI extends SwingApplication {
 				val runButton = new Button { text = "Run" }
 				val loadButton = new Button { text = "Load code" }
 				val stepButton = new Button { text = "Step "}
+				val reloadButton = new Button { text = "Reload code" }
 				val textArea =  new TextArea(io.Source.fromFile(src).mkString)
 				val lineNumber = new ListView( (1 to textArea.text.lines.length + 1).map("%02d \n".format(_))) {
 					fixedCellHeight = 15
@@ -35,6 +36,7 @@ object Micro16SimulatorGUI extends SwingApplication {
 						)	
 					}
 					contents += new BoxPanel(Orientation.Horizontal) {
+						contents += reloadButton
 						contents += loadButton
 						contents += runButton
 						contents += stepButton
@@ -42,6 +44,7 @@ object Micro16SimulatorGUI extends SwingApplication {
 					}
 				}
 
+				listenTo(reloadButton)
 				listenTo(runButton)
 				listenTo(loadButton)
 				listenTo(stepButton)
@@ -69,8 +72,12 @@ object Micro16SimulatorGUI extends SwingApplication {
 							lineNumber.selectIndices( Micro16Simulator.doStep() )
 							state.listData = State.dump().split('\n')
 						}
+					case ButtonClicked(`reloadButton`) =>
+						textArea.text = io.Source.fromFile(src).mkString
+						Micro16Simulator.loadCode(textArea.text)
+						state.listData = State.dump().split('\n')
+						lineNumber.selectIndices( 0 )
 					}
-				
 			}
 		}.open()
 	}
